41303b94d5a0926b64d79ce152a336fbe0c67c5b
Ali HamXa <alihamzatirmazi@gmail.com>
2022-03-12 16:48:29 +0500

    Update: add favicon

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-$(window).load(function () {
+$(document).ready(function () {
     $("body").removeClass("preload");
+
+    const tax = document.getElementById("tax");
+    const taxChart = new Chart(tax, {
+        type: "doughnut",
+        data: {
+            labels: ["Marketing", "Raffle", "Team", "Liquidity"],
+            datasets: [
+                {
+                    data: [3, 2, 2, 3],
+                    backgroundColor: [
+                        "rgba(171, 93, 238)",
+                        "rgb(218, 165, 32)",
+                        "rgba(29, 161, 31)",
+                        "rgb(241, 59, 59)",
+                    ],
+                    borderColor: [
+                        "rgba(171, 93, 238)",
+                        "rgb(218, 165, 32)",
+                        "rgba(29, 161, 31)",
+                        "rgb(241, 59, 59)",
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        },
+    });
 });
