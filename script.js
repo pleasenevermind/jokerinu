90b9664832dfc41c0252b4452b7623c7dce31980
Ali HamXa <alihamzatirmazi@gmail.com>
2022-03-17 18:40:13 +0500

    Update: refactor raffle.html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("body").removeClass("preload");
 
+    // main chart
     const tax = document.getElementById("tax");
     const taxChart = new Chart(tax, {
         type: "doughnut",
@@ -26,5 +27,51 @@ $(document).ready(function () {
             ],
         },
     });
-
 });
+
+// count down
+(function () {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    // milliseconds since Jan 1, 1970, 00:00:00.000 GMT
+    const countDown = new Date(Date.UTC(2022, 02, 30, 21, 0, 0)).getTime();
+    const x = setInterval(function () {
+        const now = new Date().getTime();
+        const distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(
+            distance / day
+        );
+        document.getElementById("hours").innerText = Math.floor(
+            (distance % day) / hour
+        );
+        document.getElementById("minutes").innerText = Math.floor(
+            (distance % hour) / minute
+        );
+        document.getElementById("seconds").innerText = Math.floor(
+            (distance % minute) / second
+        );
+
+        //Show live when countdown finish
+        if (distance < 0) {
+            // document.getElementById("headline").innerText = "We are live!";
+            document.getElementById("headline").style.display = "none";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "none";
+            clearInterval(x);
+        }
+        //seconds
+    }, 0);
+
+    const y = setInterval(function () {
+        document.getElementById("prize_money").innerText =
+            "$" + Math.floor(Math.random() * 500000) + 1;
+    }, 1000 * 1);
+
+    // const z = setInterval(function () {
+    //     $("#raffle-red div").attr('data-before', Math.floor(Math.random() * 5000) + 1);
+    // }, 1000 * 1);
+})();
